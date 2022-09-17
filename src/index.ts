d3062724780877808512b02a46c781170ba54ba0
John Bailey <641367+johntimothybailey@users.noreply.github.com>
2022-09-16 14:37:40 -1000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './client/astra';
-export { UserIntentRequest, CreateUserIntentResponse } from './users/intents';
+export { UserIntentRequest, CreateUserIntentResponse } from './resources/users/intents';
 export * from './lib/AstraError';
 export * from './lib/AstraResponse';
